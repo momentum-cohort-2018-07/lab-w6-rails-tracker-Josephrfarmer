12df8871fd7e0d1e3d1d7cc8108f146b53e03211
josephrfarmer <josephr.farmer24@gmail.com>
2018-09-06 20:26:16 -0400

    Routes are set up for notes working through setting up notes controller

--- beer_tracker/app/controllers/api/notes_controller.rb
+++ beer_tracker/app/controllers/api/notes_controller.rb
@@ -1,7 +1,10 @@
 class Api::NotesController < ApplicationController
-  before_action :set_beer, only: [:show, :update, :destroy,]
+  before_action :set_beer, only: [:show, :update, :destroy]
+  before_action :set_note, only: [:show, :update, :destroy]
 
-  def index  
+  def index 
+    @beer = Beer.find(params[:beer_id]) 
+    render json: @beer.notes
   end 
 
   def new
@@ -9,10 +12,15 @@ class Api::NotesController < ApplicationController
   end   
 
   def create  
-
+    @note = Note.create(
+      notes: note_params,
+      beer_id: @beer.id
+      )
+    render json: @note
   end
 
   def destroy 
+    @note.destroy
   end   
   
 
@@ -25,4 +33,7 @@ class Api::NotesController < ApplicationController
     def set_note 
       @note =Note.find(params[:id])
     end   
+
+    def note_params 
+      params.require(:note).permit(:post, :beer_id)
 end
